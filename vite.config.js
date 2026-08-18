f6ed47847e0f21c7ee8b10136e953ca2f8c1fdb7
genesisgns91 <genesisgns@gmail.com>
2026-08-18 09:51:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mug-mockup-3d/', // <-- Adicione esta linha com o nome do seu repositório
+  base: '/Mockup/', // <-- Nome exato do seu repositório no GitHub
 })
