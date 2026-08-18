7541ff7d1c83bbcc9bff33440f88642e8e30bcf1
genesisgns91 <genesisgns@gmail.com>
2026-08-18 10:26:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mockup/', // <-- Nome exato do seu repositório no GitHub
+  base: './', // <-- Altere de '/Mockup/' para './'
 })
